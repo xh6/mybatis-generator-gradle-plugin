7eb12295911c08d84e3cf3d8e7a5ea3b0b338a03
zhouxinghai <1093320666@qq.com>
2019-04-09 18:02:23 +0800

    单独

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#gradle upload  -x test --info
-#gradle clean build bintrayUpload -PbintrayUser=xh8 -PbintrayKey=fbdf3cb6c43abf8abf54fc8fb18b8637c94f95e0 -PdryRun=false -x test --debug
-
-
